b1f3f7627e1cb25b33af304e1fc0afa15e66f41f
wangguanggg <863779591@qq.com>
2020-07-09 20:59:38 +0800

    happy

--- src/hellowordl.java
+++ src/hellowordl.java
@@ -1,8 +1,11 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.sql.SQLOutput;
+
 public class hellowordl {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("the second time");
         System.out.println();
     }
 }
